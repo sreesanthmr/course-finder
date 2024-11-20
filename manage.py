ceadc449f6417740507f8517bfb3b54e506fe1c8
sreesanth <sreesanth.mr18@gmail.com>
2024-11-20 14:27:51 +0530

    jwt

--- manage.py
+++ manage.py
@@ -30,7 +30,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
