95d89d0389271cdb2c34c943c7cbbff203f5b3ab
sreesanth <sreesanth.mr18@gmail.com>
2024-11-20 20:26:47 +0530

    new

--- manage.py
+++ manage.py
@@ -1,32 +1,8 @@
-# #!/usr/bin/env python
-# """Django's command-line utility for administrative tasks."""
-# import os
-# import sys
-
-
-# def main():
-#     """Run administrative tasks."""
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-#     try:
-#         from django.core.management import execute_from_command_line
-#     except ImportError as exc:
-#         raise ImportError(
-#             "Couldn't import Django. Are you sure it's installed and "
-#             "available on your PYTHONPATH environment variable? Did you "
-#             "forget to activate a virtual environment?"
-#         ) from exc
-#     execute_from_command_line(sys.argv)
-
-#     port = int(os.environ.get("PORT", "8000"))  # Default to 8000 if PORT is not set
-#     os.system(f"python manage.py runserver 0.0.0.0:{port}")
-
-
-# if __name__ == '__main__':
-#     main()
-
-
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+
 
 def main():
     """Run administrative tasks."""
@@ -39,13 +15,37 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    execute_from_command_line(sys.argv)
 
-    if len(sys.argv) == 1 or sys.argv[1] != "runserver":
-        execute_from_command_line(sys.argv)
-    else:
-        port = os.environ.get("PORT", "8000")
-        sys.argv += [f"0.0.0.0:{port}"]
-        execute_from_command_line(sys.argv)
+    port = int(os.environ.get("PORT", "8000"))  # Default to 8000 if PORT is not set
+    os.system(f"python manage.py runserver 0.0.0.0:{port}")
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
+
+
+# import os
+# import sys
+
+# def main():
+#     """Run administrative tasks."""
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+
+#     if len(sys.argv) == 1 or sys.argv[1] != "runserver":
+#         execute_from_command_line(sys.argv)
+#     else:
+#         port = os.environ.get("PORT", "8000")
+#         sys.argv += [f"0.0.0.0:{port}"]
+#         execute_from_command_line(sys.argv)
+
+# if __name__ == "__main__":
+#     main()
